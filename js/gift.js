5c5deb7b69b423412897fea1b156a5a4533ddc12
Lucas Corrêa <bamontelucas@gmail.com>
2023-03-22 16:31:33 -0300

    mais alguns presentes

--- js/gift.js
+++ js/gift.js
@@ -1,15 +1,20 @@
 const gifts = [
+    { name: "xxxxxxx", price: 20, img: "./img/placeholder.png", paymentlink: "https://nubank.com.br/pagar/377tv/RNMI4SrDGC" },
     { name: "Cafezinho para dois", price: 25, img: "./img/cafezinho.png", paymentlink: "https://nubank.com.br/pagar/377tv/RNMI4SrDGC" },
     { name: "Almoço de rua", price: 30, img: "./img/almoco.png", paymentlink: "https://nubank.com.br/pagar/377tv/RNMI4SrDGC" },
+    { name: "xxxxxxx", price: 40, img: "./img/placeholder.png", paymentlink: "https://nubank.com.br/pagar/377tv/RNMI4SrDGC" },
+    { name: "xxxxxxx", price: 45, img: "./img/placeholder.png", paymentlink: "https://nubank.com.br/pagar/377tv/RNMI4SrDGC" },
     { name: "Caixa de doces", price: 50, img: "./img/doces.png", paymentlink: "https://nubank.com.br/pagar/377tv/RNMI4SrDGC" },
-    { name: "Piquenique no parque", price: 65, img: "./img/placeholder.png", paymentlink: "https://nubank.com.br/pagar/377tv/RNMI4SrDGC" },
+    { name: "Piquenique no parque", price: 60, img: "./img/picnic.png", paymentlink: "https://nubank.com.br/pagar/377tv/RNMI4SrDGC" },
+    { name: "xxxxxxx", price: 70, img: "./img/placeholder.png", paymentlink: "https://nubank.com.br/pagar/377tv/RNMI4SrDGC" },
     { name: "Souvenirs da viagem", price: 80, img: "./img/souvenir.jpg", paymentlink: "https://nubank.com.br/pagar/377tv/RNMI4SrDGC" },
+    { name: "xxxxxxx", price: 90, img: "./img/placeholder.png", paymentlink: "https://nubank.com.br/pagar/377tv/RNMI4SrDGC" },
     { name: "Café da manhã colonial", price: 100, img: "./img/cafe_colonial.jpg", paymentlink: "https://nubank.com.br/pagar/377tv/RNMI4SrDGC" },
     { name: "Tour guiado", price: 120, img: "./img/tour.jpg", paymentlink: "https://nubank.com.br/pagar/377tv/RNMI4SrDGC" },
     { name: "Garrafa de vinho", price: 150, img: "./img/vinho.png", paymentlink: "https://nubank.com.br/pagar/377tv/RNMI4SrDGC" },
     { name: "Entradas para o museu", price: 200, img: "./img/museum.jpg", paymentlink: "https://nubank.com.br/pagar/377tv/RNMI4SrDGC" },
     { name: "Jantar romântico", price: 250, img: "./img/jantar.jpg", paymentlink: "https://nubank.com.br/pagar/377tv/RNMI4SrDGC" },
-    { name: "Ingressos para show", price: 300, img: "./img/placeholder.png", paymentlink: "https://nubank.com.br/pagar/377tv/RNMI4SrDGC" },
+    { name: "Ingressos para show", price: 300, img: "./img/show.png", paymentlink: "https://nubank.com.br/pagar/377tv/RNMI4SrDGC" },
     { name: "xxxxxxx", price: 400, img: "./img/placeholder.png", paymentlink: "https://nubank.com.br/pagar/377tv/RNMI4SrDGC" },
     { name: "Passeio de trem", price: 500, img: "./img/trem.png", paymentlink: "https://nubank.com.br/pagar/377tv/RNMI4SrDGC" },
     { name: "Passeio aventura", price: 600, img: "./img/placeholder.png", paymentlink: "https://nubank.com.br/pagar/377tv/RNMI4SrDGC" },
@@ -19,7 +24,7 @@ const gifts = [
     { name: "Passagens aéreas para Lua de Mel", price: 1500, img: "./img/aviao.jpg", paymentlink: "https://nubank.com.br/pagar/377tv/RNMI4SrDGC" }
 ];
 
-document.querySelector('#presentes .row').insertAdjacentHTML('beforeend', gifts.sort((a, b) => a.price > b.price ? 1 : -1).map(gift => `
+document.querySelector('#presentes .row').insertAdjacentHTML('afterBegin', gifts.sort((a, b) => a.price > b.price ? 1 : -1).map(gift => `
     <div class="col col-md-6 col-lg-4 col-xl-3">
         <div class="card bg-transparent border-light my-1">
             <div class="card-header">${gift.name}</div>
